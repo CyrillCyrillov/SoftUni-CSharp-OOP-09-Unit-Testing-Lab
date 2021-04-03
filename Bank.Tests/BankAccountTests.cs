1be2e88b7aac31a75189d338877ddece207c3425
Kiril Kirilov <kirilkirilov@abv.bg>
2021-04-03 23:26:14 +0300

    Add files via upload

---
+++ Bank.Tests/BankAccountTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void WhenAcountInitilisedWithPositiveValue_AcountShouldBeChange()
+        {
+
+            decimal starAmount = 5;
+            BankAccount testAcont = new BankAccount(starAmount);
+
+            Assert.That(testAcont.Amount, Is.EqualTo(starAmount));
+
+        }
+
+        [Test]
+        public void WhenDepositMade_AmountMustIncrease()
+        {
+            decimal startAmount = 5;
+            decimal addAmount = 15;
+            BankAccount testAcount = new BankAccount(startAmount);
+
+            testAcount.Deposit(addAmount);
+
+            Assert.That(testAcount.Amount, Is.EqualTo(startAmount + addAmount));
+        }
+
+    }
+}
